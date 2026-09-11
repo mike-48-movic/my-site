2a8e891dd54f139d8d2733520c639cc419e797d3
mike-48-movic <ssekirandamike8@gmail.com>
2026-09-11 18:59:19 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const ctaBtn = document.getElementById("cta-btn");
+
+  ctaBtn.addEventListener("click", () => {
+    alert("Thanks for visiting my personal website!");
+  });
+});
